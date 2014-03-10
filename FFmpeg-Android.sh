2e516fb711a783e2ee78ffafd9e1aee7866eea66
Alex Linden Levy <lindenle@gmail.com>
2014-03-10 15:39:48 -0600

    Make sure we link to x264 when we create libffmpeg.so

--- FFmpeg-Android.sh
+++ FFmpeg-Android.sh
@@ -147,7 +147,7 @@ for version in armv7; do
   rm libavcodec/log2_tab.o
   rm libswresample/log2_tab.o
   rm libavformat/log2_tab.o
-  $CC -lm -lz -shared --sysroot=$SYSROOT -Wl,--no-undefined -Wl,-z,noexecstack $EXTRA_LDFLAGS libavutil/*.o libavutil/arm/*.o libavcodec/*.o libavcodec/arm/*.o libavformat/*.o libswresample/*.o libswscale/*.o libswscale/arm/*.o compat/*.o libswresample/arm/*.o -o $PREFIX/libffmpeg.so
+  $CC -lx264 -lm -lz -shared --sysroot=$SYSROOT -Wl,--no-undefined -Wl,-z,noexecstack $EXTRA_LDFLAGS libavutil/*.o libavutil/arm/*.o libavcodec/*.o libavcodec/arm/*.o libavformat/*.o libswresample/*.o libswscale/*.o libswscale/arm/*.o compat/*.o libswresample/arm/*.o -o $PREFIX/libffmpeg.so
 
   cp $PREFIX/libffmpeg.so $PREFIX/libffmpeg-debug.so
   arm-linux-androideabi-strip --strip-unneeded $PREFIX/libffmpeg.so
